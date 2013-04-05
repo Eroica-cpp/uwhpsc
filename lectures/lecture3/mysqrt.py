79b641a58c706fe38c5e08ae3f9016724e52dc81
Randy LeVeque <rjl@ned>
2013-04-05 09:05:31 -0700

    added convergence test

--- lectures/lecture3/mysqrt.py
+++ lectures/lecture3/mysqrt.py
@@ -1,6 +1,12 @@
-x = 9.
+x = 0.01
 s = 1.
-for k in range(6):
+kmax = 100
+tol = 1.e-14
+for k in range(kmax):
     print "Before iteration %s, s = %s" % (k,s)
+    s0 = s
     s = 0.5 * (s + x/s)
+    delta_s = s - s0
+    if abs(delta_s / x) < tol:
+        break
 print "After %s iterations, s = %s" % (k+1,s)    
