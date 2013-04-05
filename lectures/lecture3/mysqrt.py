e2888133c3da37124fdd398b7b31fa2bd19653e6
Randy LeVeque <rjl@ned>
2013-04-05 09:12:07 -0700

    converted to module and function

--- lectures/lecture3/mysqrt.py
+++ lectures/lecture3/mysqrt.py
@@ -1,12 +1,15 @@
-x = 0.01
-s = 1.
-kmax = 100
-tol = 1.e-14
-for k in range(kmax):
-    print "Before iteration %s, s = %s" % (k,s)
-    s0 = s
-    s = 0.5 * (s + x/s)
-    delta_s = s - s0
-    if abs(delta_s / x) < tol:
-        break
-print "After %s iterations, s = %s" % (k+1,s)    
+
+def sqrt2(x):
+    s = 1.
+    kmax = 100
+    tol = 1.e-14
+    for k in range(kmax):
+        print "Before iteration %s, s = %20.15f" % (k,s)
+        s0 = s
+        s = 0.5 * (s + x/s)
+        delta_s = s - s0
+        if abs(delta_s / x) < tol:
+            break
+    print "After %s iterations, s = %20.15f" % (k+1,s)  
+
+  
