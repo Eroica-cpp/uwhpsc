ac3f6e2d40431a6770664570327e457bb9460d29
Randy LeVeque <rjl@ned>
2013-04-08 08:52:16 -0700

    added debug flag, return value

--- lectures/lecture4/mysqrt.py
+++ lectures/lecture4/mysqrt.py
@@ -3,7 +3,7 @@ Module for approximating sqrt.
 More ...
 """
 
-def sqrt2(x):
+def sqrt2(x, debug=False):
     """
     more details.
     """
@@ -11,12 +11,15 @@ def sqrt2(x):
     kmax = 100
     tol = 1.e-14
     for k in range(kmax):
-        print "Before iteration %s, s = %20.15f" % (k,s)
+        if debug:
+            print "Before iteration %s, s = %20.15f" % (k,s)
         s0 = s
         s = 0.5 * (s + x/s)
         delta_s = s - s0
         if abs(delta_s / x) < tol:
             break
-    print "After %s iterations, s = %20.15f" % (k+1,s)  
+    if debug:
+        print "After %s iterations, s = %20.15f" % (k+1,s) 
+    return s 
 
   
